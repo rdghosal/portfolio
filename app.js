0b13f2c0716e91e8bd7ef2c61f500ec45998ebdc
Rahul Ghosal <rdghosal@proton.me>
2026-03-29 13:28:17 -0400

    feat(config): add pre-commit configuration and apply automatic fixes

--- app.js
+++ app.js
@@ -1,93 +1,85 @@
 function initializePage() {
-    initializeButtonLinks();
-    intializeResumeBtn();
-    initializeObservers();
+  initializeButtonLinks();
+  intializeResumeBtn();
+  initializeObservers();
 }
 
 function initializeButtonLinks() {
-    const buttonLinkMap = {
-        ".hero__btn--view": "#portfolio",
-        ".hero__btn--contact": "#contacts",
-        ".nav__home-btn": "#"
-    };
+  const buttonLinkMap = {
+    ".hero__btn--view": "#portfolio",
+    ".hero__btn--contact": "#contacts",
+    ".nav__home-btn": "#",
+  };
 
-    Object.keys(buttonLinkMap).forEach(k => {
-        let el = document.querySelector(k);        
-        el.addEventListener('click', () => {
-            window.location = `${buttonLinkMap[k]}`;
-        });
-    });  
+  Object.keys(buttonLinkMap).forEach((k) => {
+    let el = document.querySelector(k);
+    el.addEventListener("click", () => {
+      window.location = `${buttonLinkMap[k]}`;
+    });
+  });
 }
 
 function intializeResumeBtn() {
-    const btn = document.querySelector(".hero__btn--resume");
-    btn.addEventListener("click", () => {
-        window.open(
-            "./assets/Resume_Rahul D Ghosal.pdf", "_blank"
-        );
-    });
+  const btn = document.querySelector(".hero__btn--resume");
+  btn.addEventListener("click", () => {
+    window.open("./assets/Resume_Rahul D Ghosal.pdf", "_blank");
+  });
 }
 
 const containerLinkMap = {
-    "container--web": ".nav-link.web",
-    "container--mobile": ".nav-link.mobile",
-    "container--misc": ".nav-link.misc",
+  "container--web": ".nav-link.web",
+  "container--mobile": ".nav-link.mobile",
+  "container--misc": ".nav-link.misc",
 };
 
 function initializeObservers() {
+  const containers = document.querySelectorAll(".container");
+  const options = {
+    rootMargin: "-50% 0% 0% 0%",
+  };
 
-    const containers = document.querySelectorAll(".container");
-    const options = {
-        rootMargin: "-50% 0% 0% 0%"
-    };
+  const containerObserver = new IntersectionObserver((entries, containerObserver) => {
+    entries.forEach((e) => {
+      const currClass = e.target.classList[0];
 
-    const containerObserver = new IntersectionObserver((entries, containerObserver) => {
-        entries.forEach(e => {
+      if (e.isIntersecting) {
+        toggleActiveNavLink(containerLinkMap[currClass], true);
+      } else {
+        toggleActiveNavLink(containerLinkMap[currClass], false);
+      }
+    });
+  }, options);
 
-            const currClass = e.target.classList[0];
+  const cardObserver = new IntersectionObserver((entries, cardObserver) => {
+    entries.forEach((e) => {
+      if (e.isIntersecting) {
+        e.target.classList.add("display");
+        cardObserver.unobserve(e.target);
+      }
+    });
+  }, options);
 
-            if (e.isIntersecting) {
-                toggleActiveNavLink(containerLinkMap[currClass], true);
-            }
-            else {
-                toggleActiveNavLink(containerLinkMap[currClass], false);
-            }
-        });
-    }, options);
-
-    const cardObserver = new IntersectionObserver((entries, cardObserver) => {
-        entries.forEach(e => {
-
-            if (e.isIntersecting) {
-                e.target.classList.add("display");
-                cardObserver.unobserve(e.target);
-            }            
-
-        });
-    }, options);
-
-    containers.forEach((c) => {
-        containerObserver.observe(c);
-        const cards = c.querySelectorAll(".card");
-        cards.forEach((card) => {
-            cardObserver.observe(card);
-        });
-    });    
+  containers.forEach((c) => {
+    containerObserver.observe(c);
+    const cards = c.querySelectorAll(".card");
+    cards.forEach((card) => {
+      cardObserver.observe(card);
+    });
+  });
 }
 
 function toggleActiveNavLink(className, shouldActivate) {
-    const navLink = document.querySelector(className);
+  const navLink = document.querySelector(className);
 
-    if (shouldActivate) {
-        const prevLink = document.querySelector(".active");
-        if (prevLink) {
-            prevLink.classList.remove("active");
-        }
-        navLink.classList.add("active");    
+  if (shouldActivate) {
+    const prevLink = document.querySelector(".active");
+    if (prevLink) {
+      prevLink.classList.remove("active");
     }
-    else {
-        navLink.classList.remove("active");
-    }
+    navLink.classList.add("active");
+  } else {
+    navLink.classList.remove("active");
+  }
 }
 
 initializePage();
